1694cc092f8a0fd23d092c263624d174c95f79ae
Shreyasi-karan <karanshreyasi2003@gmail.com>
2026-07-21 01:44:30 +0530

    user input added

--- User-Input/user-input.sh
+++ User-Input/user-input.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+#print the line
+echo "Enter your name"
+#read the input from user and store it in variable name
+read name
+#print the name entered by user
+echo "Welcome $name"
+#print the age entered by user in variable age in one line
+read -p "Enter Age: " age
+#print the password entered by user
+read -s password
+echo "Password is $password"
